b0adef6493beb13d8d115f96e0319eba151a2901
Tommi Lew <lew.tommi@gmail.com>
2017-11-04 23:19:34 +0900

    Display entries from database
    - Update entries/index controller to retrieve all entries
    - Update entries/index view to display entries
    - Temporarily keep hardcoded entries as examples

--- apps/web/controllers/entries/index.rb
+++ apps/web/controllers/entries/index.rb
@@ -2,7 +2,10 @@ module Web::Controllers::Entries
   class Index
     include Web::Action
 
+    expose :entries
+
     def call(params)
+      @entries = EntryRepository.new.all
     end
   end
 end
