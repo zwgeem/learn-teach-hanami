455cae6fd4972b787d187da46a147d9a16acde89
Tommi Lew <lew.tommi@gmail.com>
2017-11-05 10:38:12 +0900

    Add new about-me index action
    bundle exec hanami generate action web about_me#index --url=/about-me

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -4,5 +4,6 @@
 # Example:
 # get '/hello', to: ->(env) { [200, {}, ['Hello from Hanami!']] }
 get '/', to: 'home#index'
+get '/about-me', to: 'about_me#index'
 
 resources :entries, only: [:index, :new, :create]
---
+++ apps/web/views/about_me/index.rb
@@ -0,0 +1,5 @@
+module Web::Views::AboutMe
+  class Index
+    include Web::View
+  end
+end
