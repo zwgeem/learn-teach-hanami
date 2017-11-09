4e7c7bf27aefe5b3b619a42388d97af21ba5ef3a
zwgeem <33368676+zwgeem@users.noreply.github.com>
2017-11-09 20:05:56 +0900

    least recent

--- lib/learn_teach_hanami/repositories/entry_repository.rb
+++ lib/learn_teach_hanami/repositories/entry_repository.rb
@@ -2,4 +2,7 @@ class EntryRepository < Hanami::Repository
   def most_recent
     entries.order { created_at.desc }.call
   end
+    def least_recent
+    entries.order { created_at.asc }.call
+  end
 end
